6b5ed1821cf0710aa12f36e6b8eec44746deb45c
niloofar izadyar <94452307+niloofarizadyar@users.noreply.github.com>
2021-11-25 22:18:31 +0330

    Add files via upload

---
+++ QualityMeter/main.py
@@ -0,0 +1,21 @@
+import sys
+import argparse
+from qualitymeter.qmood.extendibility import Extendability
+
+
+def main(args):
+    print("---Extendability Report---")
+    path = args.dir
+    extendabilityMeter = Extendability(path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Measures quality factors of a java project')
+    parser.add_argument(
+        '--dir',
+        help='the directory in which program source code is located')
+    args = parser.parse_args()
+    if not args.dir:
+        parser.print_help()
+        sys.exit(1)
+    main(args)
