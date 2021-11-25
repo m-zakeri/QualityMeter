833e9483575b7557e5d762cfbb2dc350b87cd2bb
a-afra <afraaalireza@gmail.com>
2021-11-25 15:12:35 +0330

    NOP metric for flexibility measurement added.

--- qualitymeter/qmood/flexibility.py
+++ qualitymeter/qmood/flexibility.py
@@ -1,6 +1,33 @@
-"""
+from antlr4 import *
+from qualitymeter.gen.javaLabeled.JavaLexer import JavaLexer
+from qualitymeter.gen.javaLabeled.JavaParserLabeled import JavaParserLabeled
+from qualitymeter.gen.javaLabeled.JavaParserLabeledListener import JavaParserLabeledListener
 
 
-"""
+# listener for NOP metric
+class polymorphismListener(JavaParserLabeledListener):
 
-# Todo: implement the flexibility module
+    def __init__(self):
+        self.__number_of_extended_classes = 0
+
+    @property
+    def get_number_of_extended_classes(self):
+        return self.__number_of_extended_classes
+
+    # count the number of polymorphic methods
+    def enterClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
+        # check if method overriding occurs
+        if ctx.EXTENDS() is not None:
+            self.__number_of_extended_classes += 1
+
+
+if __name__ == '__main__':
+    stream = FileStream("your file path", encoding='utf-8')
+    lexer = JavaLexer(stream)
+    token_stream = CommonTokenStream(lexer)
+    parser = JavaParserLabeled(token_stream)
+    pars_tree = parser.compilationUnit()
+    my_listener = polymorphismListener()
+    walker = ParseTreeWalker()
+    walker.walk(t=pars_tree, listener=my_listener)
+    print("Number of Polymorphic Methods:", my_listener.get_number_of_extended_classes)
