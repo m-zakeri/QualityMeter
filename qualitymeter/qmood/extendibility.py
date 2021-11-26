2592d8b8234d9bc1f16a84a102cb805250b7df34
niloofar izadyar <94452307+niloofarizadyar@users.noreply.github.com>
2021-11-26 12:33:25 +0330

    Update extendibility.py

--- qualitymeter/qmood/extendibility.py
+++ qualitymeter/qmood/extendibility.py
@@ -3,17 +3,20 @@
 
 """
 from .metrics.polymorphism import Polymorphism
+from .metrics.coupling import Coupling
 
 class Extendability:
     def __init__(self, projectPath):
         self.projectPath = projectPath
         polymorphismValue = self.calcPolymorphism()
+        couplingValue = self.calcCoupling()
 
     def calcAbstraction(self):
         pass
 
     def calcCoupling(self):
-        pass
+        couplingMeter = Coupling(self.projectPath)
+        return couplingMeter.calcCoupling()
 
     def calcInheritance(self):
         pass
