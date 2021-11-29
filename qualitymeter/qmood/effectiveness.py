a1974ffd59b11dbb10d4f523f2bdec2210b8a3c1
KTeymoury <95079902+KTeymoury@users.noreply.github.com>
2021-11-29 10:24:46 +0330

    Added comment and DocStrings to code
    This commit adds the the comments and stuff.
    Caution: Bad English lies ahead!

--- qualitymeter/qmood/effectiveness.py
+++ qualitymeter/qmood/effectiveness.py
@@ -4,30 +4,45 @@ from qualitymeter.gen.javaLabeled.JavaParserLabeled import JavaParserLabeled
 from qualitymeter.gen.javaLabeled.JavaParserLabeledListener import JavaParserLabeledListener
 from effectiveness_utils import InfoExtractorListener, get_list_of_files
 
-
+# A dictionary to speed up class look ups using class name
 __searchCache = {}
+# A set of user-defined types seen across the code
 __userDefined = set()
+# A list of parsed classes
 __allClasses = []
+# A list of classes that extend another one
 __inheritingClasses = []
 
 
 def calculate(path):
+    """
+    Calculates the effectiveness metric of a java project.
+
+    :param path: Source path of the codebase
+    :return: Effectiveness metric
+    """
+    # Pull globals in
     global __searchCache
     global __userDefined
     global __allClasses
     global __inheritingClasses
+    # Initiate necessary metrics
     ANA = 0
     DAM = 0
     MOA = 0
     MFA = 0
     NOP = 0
+    # Get a list of .java files
     files = get_list_of_files(path)
+    # Logs
     print(f'Number of files detected: {len(files)}')
     print('Proceeding to parse...')
+    # Parse the files and update the globals
     _parse_files(files)
+    # Handle inheritance
     for item in __inheritingClasses:
         _set_ancestors(item)
-
+    # Calculate project-wise metrics
     for item in __allClasses:
         item['MOA'] = _calc_MOA(item.pop('class_fields'))
         ANA += item['ANA']
@@ -39,68 +54,110 @@ def calculate(path):
 
 
 def _parse_files(file_list):
+    """
+    Parse all of the .java files in the specified directory and all of its subdirectories
+    and updates the global parameters.
+    """
+    # Pull globals in
     global __searchCache
     global __userDefined
     global __allClasses
     global __inheritingClasses
-
+    # Reset the globals
     __searchCache = {}
     __userDefined = set()
     __allClasses = []
     __inheritingClasses = []
-
+    # Initiate a walker
     walker = ParseTreeWalker()
+    # Iterate over all of the indexed .java files and parse them
     for file in file_list:
+        # Generic ANTLR stuff
         input_stream = FileStream(file, encoding="utf-8")
         lexer = JavaLexer(input_stream)
         stream = CommonTokenStream(lexer)
         parser = JavaParserLabeled(stream)
         tree = parser.compilationUnit()
+        # Initiate a custom listener
         extractor = InfoExtractorListener()
+        # Walk the tree using listener
         walker.walk(extractor, tree)
+        # Update globals
         __userDefined.update(extractor.return_user_defined())
         for item in extractor.return_indexed_classes():
+            # Update search cache
             if not item['name'] in __searchCache:
                 __searchCache[item['name']] = item
-            '''else:
-                print(f'duplicate class {item["name"]}')'''
+            # Update list of all classes
             __allClasses.append(item)
+            # Mark classes with a parent as inheriting
             if 'parent' in item:
                 __inheritingClasses.append(item)
 
 
 def _set_ancestors(item):
+    """
+    Traces back class parents recursively and updates related metrics.
+
+    :param item: Class info dictionary
+    """
+    # Cache the parent parameter
     tmp = item['parent']
+    # If it's already seen, return
     if type(tmp) != str:
         return
+    # If the inherited class is not in the indexed scope, return
     if tmp.endswith('+'):
         return
+    # If parent is indexed, update the parent parameter to point at the class
     if item['parent'] in __searchCache:
         item['parent'] = __searchCache[tmp]
+    # If it's not indexed mark it with a '+' sign and update the relevant class metrics
     else:
         item['parent'] += '+'
         item['MFA'] = 0
         item['ANA'] = 1
         item['NOP'] = len(item['inheritable_methods']) - item['final_method_count']
         return
+    # If the parent itself inherits a class recursively handle that too
     if 'parent' in item['parent']:
-        _set_ancestors(item['parent'])
+        _set_ancestors(item['parent'])  # recursion
+    # Calculate the number of un-inheritable methods
     own = item['method_count'] - len(item['inheritable_methods'])
+    # Inherit the methods from parent
     item['inheritable_methods'].update(item['parent']['inheritable_methods'])
+    # Update the final method counter of current class
     item['final_method_count'] += item['parent']['final_method_count']
+    # Cache the number of inheritable methods
     tmp = len(item['inheritable_methods'])
+    # Update the polymorphism metric
+    # (Note: by default, any inheritable method in java is polymorphic except for the final ones)
     item['NOP'] = tmp - item['final_method_count']
+    # Calculate the number of methods using private and inheritable ones
     tmp += own
+    # Update the class method counter
     item['method_count'] = tmp
+    # Update MFA values
+    # Handle division by zero
     if tmp == 0:
         item['MFA'] = 1
+    # Divide inherited methods by total method to get MFA
     else:
         item['MFA'] = len(item['parent']['inheritable_methods']) / tmp
+    # Update ANA
+    # Note: since java doesn't support multiple inheritance we just increment parent's ANA by one
     item['ANA'] = item['parent']['ANA'] + 1
 
 
 def _calc_MOA(names):
+    """
+    Calculates MOA metric.
+
+    :param names: A list of non-primitive fields in a class
+    :return: Number of user-defined fields
+    """
     count = 0
+    # Iterate over non primitive data types and check and see if they are user-defined
     for item in names:
         if item in __userDefined:
             count += 1
@@ -108,6 +165,9 @@ def _calc_MOA(names):
 
 
 if __name__ == '__main__':
+    # Get the code base address from user
     directory = input('Please enter your code base address:\n')
+    # Calculate effectiveness
     effectiveness = calculate(directory)
+    # Print that for user
     print(f'Effectiveness: {effectiveness}\nNumber of classes: {len(__allClasses)}')
--- qualitymeter/qmood/effectiveness_utils.py
+++ qualitymeter/qmood/effectiveness_utils.py
@@ -6,15 +6,22 @@ from qualitymeter.gen.javaLabeled.JavaParserLabeledListener import JavaParserLab
 
 
 class InfoExtractorListener(JavaParserLabeledListener):
+    """
+    A custom listener based on ANTLR generated one, to extract necessary information about classes.
+    """
+
     def __init__(self):
+        """
+        Create a new InfoExtractorListener to use with ANTLR tree walker.
+        """
         self.__user_defined = set()
-        self.__class_info = None
-        self.__class_stack = []
-        self.__depth = 0
+        self.__class_info = None  # the class which is currently being walked
+        self.__class_stack = []  # list of seen classes
+        self.__depth = 0  # to keep track of nested classes
         self.__max_depth = 0
-        self.__stack_pointers = []
+        self.__stack_pointers = []  # to keep track of stashed classes
         self.__current_pointer = -1
-        self.__uninheritable_tags = ('static', 'private')
+        self.__uninheritable_tags = ('static', 'private')  # a list of modifiers tha block inheritance in java
 
     def enterClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
         self.__user_defined.add(ctx.IDENTIFIER().getText())
@@ -77,19 +84,35 @@ class InfoExtractorListener(JavaParserLabeledListener):
         tmp = ctx.fieldDeclaration().typeType().classOrInterfaceType()
         if tmp:
             text = tmp.getText()
+            # Ignore the common String and Random types
             if text != 'String' or text != 'Random':
                 self.__class_info['class_fields'].append(text)
 
     def return_indexed_classes(self):
+        """
+        Returns a list of classes indexed by info extractor.
+
+        :return: List of class info dictionaries
+        """
         return self.__class_stack
 
     def return_user_defined(self):
+        """
+        Returns a list of user-defined types, parsed by the info extractor.
+        This list includes classes and enums.
+
+        :return: List of user-defined types
+        """
         return self.__user_defined
 
 
 def get_list_of_files(dir_name):
-    # create a list of file and sub directories
-    # names in the given directory
+    """
+    Lists all of the .java files in the directory and all of the subdirectories.
+
+    :param dir_name: Base directory address
+    :return: A list consisting of full-path to the java files
+    """
     list_of_files = os.listdir(dir_name)
     all_files = list()
     # Iterate over all the entries
@@ -99,6 +122,7 @@ def get_list_of_files(dir_name):
         # If entry is a directory then get the list of files in this directory
         if os.path.isdir(full_path):
             all_files = all_files + get_list_of_files(full_path)
+        # If entry is a .java file add it to the list
         elif entry.lower().endswith('.java'):
             all_files.append(full_path)
     return all_files
