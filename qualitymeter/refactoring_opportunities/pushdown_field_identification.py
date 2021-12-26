b5a7cd648a825711c53b38da4cb2559e94b681cb
m-zakeri <m-zakeri@live.com>
2021-12-26 15:08:01 +0330

    Add pushdown_field_identification.py and pushdown_field_identification.py

---
+++ qualitymeter/refactoring_opportunities/pushdown_field_identification.py
@@ -0,0 +1,6 @@
+"""
+The module identify push-down field refactoring opportunities in Java projects
+
+"""
+
+# Todo: Implementing a decent push-down field identification algorithm.
---
+++ qualitymeter/refactoring_opportunities/pushdown_method_identification.py
@@ -0,0 +1,6 @@
+"""
+The module identify push-down method refactoring opportunities in Java projects
+
+"""
+
+# Todo: Implementing a decent push-down method identification algorithm.
