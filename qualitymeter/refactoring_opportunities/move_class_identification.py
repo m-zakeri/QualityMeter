6adfb015351341b41b56fdba9b402adedc45dba8
m-zakeri <m-zakeri@live.com>
2021-12-26 14:38:09 +0330

    Add move_class_identification.py and move_field_identification.py modules

---
+++ qualitymeter/refactoring_opportunities/move_class_identification.py
@@ -0,0 +1,6 @@
+"""
+The module identify move class refactoring opportunities in Java projects
+
+"""
+
+# Todo: Implementing a decent move class identification algorithm.
---
+++ qualitymeter/refactoring_opportunities/move_field_identification.py
@@ -0,0 +1,6 @@
+"""
+The module identify move field refactoring opportunities in Java projects
+
+"""
+
+# Todo: Implementing a decent move field identification algorithm.
