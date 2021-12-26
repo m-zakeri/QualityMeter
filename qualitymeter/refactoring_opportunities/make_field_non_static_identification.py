a702ccdd25451a7272779bd8eabc6616e291d71e
m-zakeri <m-zakeri@live.com>
2021-12-26 14:37:23 +0330

    Add make_field_static_identification.py and make_field_non_static_identification.py modules

---
+++ qualitymeter/refactoring_opportunities/make_field_non_static_identification.py
@@ -0,0 +1,6 @@
+"""
+The module identify make field non-static opportunities in Java projects
+
+"""
+
+# Todo: Implementing a decent make field non-static identification algorithm.
---
+++ qualitymeter/refactoring_opportunities/make_field_static_identification.py
@@ -0,0 +1,6 @@
+"""
+The module identify make field static opportunities in Java projects
+
+"""
+
+# Todo: Implementing a decent make field static identification algorithm.
