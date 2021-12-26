db73bdd119c11db741f09b86744c4462e449a97a
m-zakeri <m-zakeri@live.com>
2021-12-26 14:40:08 +0330

    Add make_method_static_identification.py and make_method_non_static_identification.py modules

---
+++ qualitymeter/refactoring_opportunities/make_method_non_static_identification.py
@@ -0,0 +1,6 @@
+"""
+The module identify make method non-static opportunities in Java projects
+
+"""
+
+# Todo: Implementing a decent make method non-static identification algorithm.
---
+++ qualitymeter/refactoring_opportunities/make_method_static_identification.py
@@ -0,0 +1,6 @@
+"""
+The module identify make method static opportunities in Java projects
+
+"""
+
+# Todo: Implementing a decent make method static identification algorithm.
